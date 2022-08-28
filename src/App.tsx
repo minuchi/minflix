eeb0fa64890f1eafd6c27fd84cd4255b4670b994
minuchi <kimkimee2@gmail.com>
2022-08-28 15:16:26 +0900

    Set up to deploy to github page

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import Header from './Components/Header';
 import Home from './Routes/Home';
 import Search from './Routes/Search';
 import Tv from './Routes/Tv';
+import { getPathnameFromURL } from './utils';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={getPathnameFromURL(process.env.PUBLIC_URL)}>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import parse from 'url-parse';
+
 export function makeImgUrl(id: string, format: string = 'original') {
   return `https://image.tmdb.org/t/p/${format}${id}`;
 }
@@ -5,3 +7,12 @@ export function makeImgUrl(id: string, format: string = 'original') {
 export function summarizeText(text: string, size: number = 200) {
   return text.length > size ? text.slice(0, size) + '...' : text;
 }
+
+export function getPathnameFromURL(url?: string) {
+  if (!url) {
+    return '/';
+  }
+  const { pathname } = parse(url);
+
+  return pathname;
+};
